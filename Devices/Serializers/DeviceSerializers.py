dadbfe53178a3b70f843ca3e96022ce398cf88c3
mahziar <ahmadian.mahziyar@gmail.com>
2024-01-04 00:35:09 +0330

    create ThreadHandler class to make thread for user_time_duration .

--- Devices/Serializers/DeviceSerializers.py
+++ Devices/Serializers/DeviceSerializers.py
@@ -1,3 +1,5 @@
+import threading
+from datetime import datetime, timedelta
 from Devices.models.devices import Devices, DeviceTypes
 from Authorization.models import Admins, Users
 from Authorization.TokenManager import user_id_to_token, token_to_user_id
@@ -6,7 +8,87 @@ from Authorization.Serializers.AdminSerilizer import AdminSerializers
 from MQQTService.Publisher import publish_message_to_client
 
 
+class ThreadHandler:
+    def __init__(self):
+        self.__alive_device = []
+        self.__devices_start_time = {}
+
+    def __real_thread(self, device_serial, time_to_shout_down):
+        device_object = Devices.objects.get(serial=device_serial)
+        user_id = device_object.user.id
+        while True:
+            if device_serial in self.__alive_device:
+                current_time = datetime.now()
+                if current_time.strftime('%Y-%m-%d %H:%M:%S') == time_to_shout_down.strftime('%Y-%m-%d %H:%M:%S'):
+                    # publish turn off to mqtt broker
+                    prepared_data = {
+                        "serial": device_object.serial,
+                        "type_name": device_object.type.name,
+                        "state": False,
+                    }
+                    publish_message_to_client(data=prepared_data)
+                    Users.objects.filter(id=user_id).update(time_duration=0)
+                    self.__alive_device.remove(device_serial)
+                    if device_serial in list(self.__devices_start_time.keys()):
+                        self.__devices_start_time.pop(device_serial)
+                    break
+            else:
+                break
+
+    def start_thread(self, device_serial):
+        device_object = Devices.objects.get(serial=device_serial)
+        self.__alive_device.append(device_serial)
+        current_time = datetime.now()
+        self.__devices_start_time[f'{device_serial}'] = current_time
+        user_time_duration = device_object.user.time_duration
+        time_to_shout_down = current_time + timedelta(seconds=user_time_duration)
+        prepared_data = {
+            "serial": device_object.serial,
+            "type_name": device_object.type.name,
+            "state": True,
+        }
+        publish_message_to_client(data=prepared_data)
+        t = threading.Thread(target=self.__real_thread, args=(device_serial, time_to_shout_down))
+        t.start()
+
+    def stop_thread(self, device_serial):
+        current_time = datetime.now()
+        device_object = Devices.objects.get(serial=device_serial)
+        user_id = device_object.user.id
+        if device_serial in self.__alive_device:
+            self.__alive_device.remove(device_serial)
+        if device_serial in list(self.__devices_start_time.keys()):
+            user_start_time = self.__devices_start_time[device_serial]
+            time_difference = (current_time - user_start_time).total_seconds()
+            if time_difference > 0:
+                user_object = Users.objects.filter(id=user_id)
+                user_time_duration = list(user_object.values('time_duration'))[0]['time_duration']
+                time_delta = user_time_duration - time_difference
+                if time_delta > 0:
+                    user_object.update(time_duration=time_delta)
+                    # publish turn off to mqtt broker
+                    prepared_data = {
+                        "serial": device_object.serial,
+                        "type_name": device_object.type.name,
+                        "state": False,
+                    }
+                    publish_message_to_client(data=prepared_data)
+                elif time_delta <= 0:
+                    user_object.update(time_duration=0)
+
+        if device_serial not in self.__alive_device and device_serial not in list(self.__devices_start_time.keys()):
+            # This state occurs when server is restart
+            print("server restarted")
+            prepared_data = {
+                "serial": device_object.serial,
+                "type_name": device_object.type.name,
+                "state": False,
+            }
+            publish_message_to_client(data=prepared_data)
+
+
 class DeviceSerializers:
+    thread_object = ThreadHandler()
 
     @staticmethod
     def admin_create_serializer(token, name, serial, type_id, other_information):
@@ -150,7 +232,18 @@ class DeviceSerializers:
             return False, wrong_token_result
 
     @staticmethod
-    def user_send_order_serializer(token, serial , state):
+    def thread_for_use_time_duration(device_serial, user_time_duration):
+        current_time = datetime.now()
+        time_to_shout_down = current_time + timedelta(minutes=user_time_duration)
+        while True:
+            current_time = datetime.now()
+            if current_time == time_to_shout_down:
+                # send trun off to device
+                pass
+        pass
+
+    @staticmethod
+    def user_send_order_serializer(token, serial, state):
         token_result = token_to_user_id(token)
         if token_result["status"] == "OK":
             user_id = token_result["data"]["user_id"]
@@ -165,12 +258,16 @@ class DeviceSerializers:
             if str(device_object.user.id) != str(user_id):
                 wrong_data_result["message"] = "Invalid data"
                 return False, wrong_data_result
-            prepared_data = {
-                "serial": device_object.serial,
-                "type_name": device_object.type.name,
-                "state": state,
-            }
-            publish_message_to_client(data=prepared_data)
+            user_time_duration = device_object.user.time_duration
+            # start_thread
+            if user_time_duration <= 0:
+                wrong_data_result["message"] = "Please buy time."
+                return False, wrong_data_result
+            elif user_time_duration > 0:
+                if state is True:
+                    DeviceSerializers.thread_object.start_thread(device_serial=serial)
+                elif state is False:
+                    DeviceSerializers.thread_object.stop_thread(device_serial=serial)
             return True, status_success_result
         else:
             return False, wrong_token_result
@@ -187,9 +284,9 @@ class DeviceSerializers:
                     queryset = user_devices.order_by('-create_date')
                     user_response = Devices.objects.serialize(queryset=queryset)
                     prepared_data = {
-                        "UserID":id ,
-                        "AllUserDeviceCount":user_devices.count() ,
-                        "AllUserDevices" : user_response
+                        "UserID": id,
+                        "AllUserDeviceCount": user_devices.count(),
+                        "AllUserDevices": user_response
                     }
                     response.append(prepared_data)
                 return True, response
